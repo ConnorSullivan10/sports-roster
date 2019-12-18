ee16eb4c87626e7518847eb7d1d03cdc7c6d983f
Connor Sullivan <cksullvn@gmail.com>
2019-12-18 14:59:06 -0600

    WIP

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,30 +1,19 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import authData from '../../helpers/data/authData';
-import playerData from '../../helpers/data/playerData';
+import PropTypes from 'prop-types';
 import Player from '../Player/Player';
+import playerShape from '../../helpers/props/playerShape';
 
 class Team extends React.Component {
-  state = {
-    players: [],
-  }
-
-  componentDidMount() {
-    this.getPlayers();
-  }
-
-  getPlayers = () => {
-    playerData.getPlayersByUid(authData.getUid())
-      .then((players) => {
-        this.setState({ players });
-      })
-      .catch((errFromTeam) => console.error({ errFromTeam }));
+  static propTypes = {
+    players: PropTypes.arrayOf(playerShape.playerShape),
   }
 
   render() {
+    const myPlayers = this.props.players;
+    const playerCards = myPlayers.map((player) => <Player key={player.id} player={player} />);
     return (
       <div>
-        {this.state.players.map((player) => (<Player key={player.id} player={player} />))}
+        {playerCards}
       </div>
     );
   }
